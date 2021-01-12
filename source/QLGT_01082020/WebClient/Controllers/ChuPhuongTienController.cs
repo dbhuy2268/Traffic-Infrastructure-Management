d69e80950d1e7b45e4c6f12f3426cf3ab0d9a791
Phan Dang Khoa <58247911+PhanDangKhoa1712536@users.noreply.github.com>
2021-01-12 23:15:53 +0700

    Add ChuPhuongTienController, ThongTinLichHenController, ThongTinThanhToanController
    Remaining: GPLXcontroller, LuatController
    Completion: 90%

---
+++ source/QLGT_01082020/WebClient/Controllers/ChuPhuongTienController.cs
@@ -0,0 +1,49 @@
+﻿using DataRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebClient.Controllers
+{
+    public class ChuPhuongTienController : ApiController
+    {
+        // GET: api/ChuPhuongTienController
+        public List<ChuPhuongTien> Get()
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            return context.ChuPhuongTiens.ToList();
+        }
+
+        // GET: api/ChuPhuongTien/5
+        public ChuPhuongTien Get(int id)
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            return context.ChuPhuongTiens.Find(id);
+        }
+
+        // POST: api/ChuPhuongTien
+        public void Post([FromBody] ChuPhuongTien obj)
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            context.ChuPhuongTiens.Add(obj);
+            context.SaveChanges();
+        }
+
+        // PUT: api/ChuPhuongTien/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ChuPhuongTien/5
+        public void Delete(int id)
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            var data = context.ChuPhuongTiens.ToList().Where(x => x.id == id).First();
+            context.ChuPhuongTiens.Remove(data);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ source/QLGT_01082020/WebClient/Controllers/ThongTinLichHenController.cs
@@ -0,0 +1,50 @@
+﻿using DataRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebClient.Controllers
+{
+    public class ThongTinLichHenController : ApiController
+    {
+        // GET: api/ThongTinLichHen
+        public List<ThongTinLichHen> Get()
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            var lstYeuCau = context.ThongTinLichHens.ToList();
+            return lstYeuCau;
+        }
+
+        // GET: api/ThongTinLichHen/5
+        public ThongTinLichHen Get(int id)
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            return context.ThongTinLichHens.Find(id);
+        }
+
+        // POST: api/ThongTinLichHen
+        public void Post(ThongTinLichHen obj)
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            context.ThongTinLichHens.Add(obj);
+            context.SaveChanges();
+        }
+
+        // PUT: api/ThongTinLichHen/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ThongTinLichHen/5
+        public void Delete(int id)
+        {
+            QLGTDbContext context = new QLGTDbContext();
+            var data = context.ThongTinLichHens.ToList().Where(x => x.id == id).First();
+            context.ThongTinLichHens.Remove(data);
+            context.SaveChanges();
+        }
+    }
+}
