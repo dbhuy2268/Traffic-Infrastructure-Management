9d1e8f9e8aa1180589506f616ad3161d34e89d05
Phan Dang Khoa <58247911+PhanDangKhoa1712536@users.noreply.github.com>
2021-01-14 00:03:37 +0700

    Add compulsory controller for Dang Ky Xe function

---
+++ source/QLGT_01082020/TheFront/Controllers/HoSoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TheFront.Models;
+using TheFront.Helper;
+
+
+namespace TheFront.Controllers
+{
+    public class HoSoController : Controller
+    {
+        private readonly ILogger<HoSoController> _logger;
+
+        public HoSoController(ILogger<HoSoController> logger)
+        {
+            _logger = logger;
+        }
+        //public IActionResult Index()
+        //{
+        //    //return View();
+        //    return View("~/Views/Home/Index.cshtml");
+        //}
+
+        TheAPI _api = new TheAPI();
+        
+        [HttpPost]
+        public async Task<HoSoModel> Create_HoSo(HoSoModel o_HoSo)
+        {
+            HoSoModel HS = new HoSoModel();
+            HttpClient client = _api.Initial();
+
+            using (client)
+            {
+                //Http Post
+                using (var postTask = await client.PostAsync("api/BienBan", new StringContent(JsonConvert.SerializeObject(o_HoSo), Encoding.UTF8, "application/json")))
+                {
+                    var api_response = await postTask.Content.ReadAsStringAsync();
+                    HS = JsonConvert.DeserializeObject<HoSoModel>(api_response);
+                }
+            }
+            //return RedirectToAction("Create");
+            return HS;
+        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
