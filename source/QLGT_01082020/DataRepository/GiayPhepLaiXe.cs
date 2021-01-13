e8472633064a4d42f046f816d81de662572f8984
Huy Ngo <huyngo057@gmail.com>
2021-01-13 09:17:09 +0700

    Add GPLX controller + data_repo

---
+++ source/QLGT_01082020/DataRepository/GiayPhepLaiXe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace DataRepository
+{
+   public class GiayPhepLaiXe
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public Nullable<System.DateTime> NgaySinh { get; set; }
+        public string NgheNghiep { get; set; }
+        public string CMND { get; set; }
+        public Nullable<System.DateTime> NgayCap { get; set; }
+        public string NoiCap { get; set; }
+        public string SoDienThoai { get; set; }
+        public string QuocTich { get; set; }
+    }
+}
