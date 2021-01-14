4d9386dcdb3eccb12dda8147d61f2fcfdc596522
Huy Ngo <huyngo057@gmail.com>
2021-01-14 14:46:20 +0700

    Edit TheFront-Controller

--- source/QLGT_01082020/TheFront/Controllers/HomeController.cs
+++ source/QLGT_01082020/TheFront/Controllers/HomeController.cs
@@ -98,18 +98,7 @@ namespace TheFront.Controllers
         }
 
 
-        public async Task<IActionResult> Home_GPLX()
-        {
-            List<GiayPhepLaiXeModel> gplx = new List<GiayPhepLaiXeModel>();
-            HttpClient client = _api.Initial();
-            HttpResponseMessage res = await client.GetAsync("api/GiayPhepLaiXe");
-            if (res.IsSuccessStatusCode)
-            {
-                var results = res.Content.ReadAsStringAsync().Result;
-                gplx =  JsonConvert.DeserializeObject<List<GiayPhepLaiXeModel>>(results);
-            }
-            return View(gplx);
-        }
+       
 
         public ActionResult Create_BB()
         {
